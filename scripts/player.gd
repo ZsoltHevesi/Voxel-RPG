2039d34c97f241170b9d873f0818258b7fbf3ca2
ZsoltHevesi <c22387231@mytudublin.ie>
2024-01-31 21:45:52 +0000

    Init

---
+++ scripts/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+@onready var camera_mount = $cameraMount
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 6
+
+@export var mouseSensitivity = 0.3
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+# Capture Mouse
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+# Rotate Player and Camera
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(- event.relative.x * mouseSensitivity))
+		camera_mount.rotate_x(deg_to_rad(- event.relative.y * mouseSensitivity))
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
