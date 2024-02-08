23a8405cb7f6ae12329fac0f20a56b0aa1914f29
ZsoltHevesi <c22387231@mytudublin.ie>
2024-02-08 22:44:12 +0000

    finding way to prevent player collision rotation

--- scripts/playerCollision3D.gd
+++ scripts/playerCollision3D.gd
@@ -1,13 +1,9 @@
 extends CollisionShape3D
 
-var parent_rotation
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	parent_rotation = get_parent().rotation
-	set_rotation(- parent_rotation)
+	pass
