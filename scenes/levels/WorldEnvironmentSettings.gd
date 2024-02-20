ddaf61d259ccdfc8ff51e2d8d41fa83e38567095
Georgy Kachelkin <d22124642@mytudublin.ie>
2024-02-20 20:21:41 +0000

    added working visual settings

---
+++ scenes/levels/WorldEnvironmentSettings.gd
@@ -0,0 +1,13 @@
+extends WorldEnvironment
+
+
+func _ready():
+	if Graphics.ssao == 1:
+		environment.ssao_enabled = true
+	else:
+		environment.ssao_enabled = false
+
+	if Graphics.glow == 1:
+		environment.glow_enabled = true
+	else:
+		environment.glow_enabled = false
---
+++ scripts/settings/graphics.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var shadows = 0
+var ssao = 0
+var glow = 0
