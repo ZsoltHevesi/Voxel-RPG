6ed68374bd1c9d1b7c69c55576ac2efff43d6bc6
Georgy Kachelkin <d22124642@mytudublin.ie>
2024-02-22 03:53:48 +0000

    centered menus, added wip resolution code, removed visual settings for now

--- scenes/levels/DirectionalLight3DSettings.gd
+++
@@ -1,7 +0,0 @@
-extends DirectionalLight3D
-
-func _ready():
-	if Graphics.shadows == 1:
-		shadow_enabled = true
-	else:
-		shadow_enabled = false
--- scripts/settings/graphics.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-var shadows = 0
-var ssao = 0
-var glow = 0
-var scaling = 1
