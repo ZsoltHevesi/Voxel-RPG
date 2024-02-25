f174a025c40cac9d4868af0f87cfb7c5fed4e390
Daniel O'Neill <danieloneill18@yahoo.com>
2024-02-25 23:18:13 +0000

    altered enemy speed, reorganised enemy script

--- scenes/enemies/clanger/e_clanger.gd
+++ scenes/enemies/clanger/e_clanger.gd
@@ -1,14 +1,29 @@
 extends CharacterBody3D
 
-var speed = 5
+var speed = 6
 var accel = 10
 var chaseRange = 15
-
-
-
-
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 @onready var player = $"../Player"
+
+var maxHealth = 100
+var currentHealth = maxHealth
+
+func takeDamage(amount):
+	currentHealth -= amount
+	if currentHealth <= 0:
+		# Function to handle player death
+		die()
+
+func die():
+	# Implementing logic for player death (death animations, resetting health etc.)
+	currentHealth = maxHealth
+
+func heal(amount):
+	currentHealth += amount
+	if currentHealth > maxHealth:
+		currentHealth = maxHealth
+
 
 func _ready():
 
@@ -18,6 +33,12 @@ func _ready():
 		print("Player not found or assigned.")
 
 func _physics_process(delta):
+	# Example: Check for player health and take damage if needed
+	if Input.is_action_just_pressed("takeDamage"):
+		# Amount of damage can be adjusted as needed
+		takeDamage(20) 
+		
+		
 	# AI pathfinding logic
 	if player:
 
