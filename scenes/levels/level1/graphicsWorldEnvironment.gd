f9351830bf50bc6a56943991c5e877012cb28540
Georgy Kachelkin <d22124642@mytudublin.ie>
2024-02-29 17:08:53 +0000

    settings work again

---
+++ scenes/levels/level1/graphicsWorldEnvironment.gd
@@ -0,0 +1,8 @@
+extends WorldEnvironment
+
+@onready var config = ConfigFile.new()
+
+func _ready():
+	config.load("user://config.cfg")
+	environment.ssao_enabled = config.get_value("options", "ssao")
+	environment.glow_enabled = config.get_value("options", "glow")
