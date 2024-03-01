333d4a9900417f457dd3ba509267fb85b887e4c5
Georgy Kachelkin <d22124642@mytudublin.ie>
2024-03-01 13:41:56 +0000

    fixed config not existing

--- AutoLoad/config.gd
+++ AutoLoad/config.gd
@@ -5,7 +5,17 @@ var config = ConfigFile.new()
 func _ready():
 	var err = config.load("user://config.cfg")
 	if err != OK:
-		return
+		config.set_value("options", "fullscreen", false)
+		config.set_value("options", "vsync", true)
+		config.set_value("options", "ssao", true)
+		config.set_value("options", "glow", true)
+		config.set_value("options", "scaling_mode", 1)
+		config.set_value("options", "scaling_value", 100.00)
+		config.set_value("options", "resolution_width", 1920)
+		config.set_value("options", "resolution_height", 1080)
+		config.set_value("options", "fsr", 0)
+		config.set_value("options", "shadows", true)
+		config.save("user://config.cfg")
 	get_window().set_size(Vector2i(config.get_value("options", "resolution_width"), config.get_value("options", "resolution_height")))
 	
 	if config.get_value("options", "fullscreen"):
